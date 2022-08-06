0cf6764ce5ff5bd2b0d6d745c42dbbcecf246d64
yeti2006 <senukabandara2020@outlook.com>
2022-08-06 19:57:52 +0530

    Fix  and grab token from env

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,8 @@
 from configparser import ConfigParser
 from loguru import logger
+import os
+
+DEFAULT_MESSAGE_FORMAT = "**{author}**(_{server}_) - {time}{newline}{message.content}"
 
 
 class Configuration:
@@ -8,14 +11,18 @@ class Configuration:
         self.parser.read("./config.ini")
 
     def get_discord_token(self) -> str:
-        token = self.parser["info"]["discord_account_token"]
+        token = str(self.parser["info"]["discord_account_token"])
+        if token == "your_account_token":
+            token = os.environ.get("discord_account_token")
 
-        return token
+        return str(token)
 
     def get_log_channel(self) -> int:
         channel_id = int(self.parser["info"]["logs_channel_id"])
+        message_format = str(self.parser["info"]["message_format"])
+
         return (
-            (channel_id, str(self.parser["info"]["message_format"]))
+            (channel_id, message_format if message_format else DEFAULT_MESSAGE_FORMAT)
             if not channel_id == 0
             else None
         )
@@ -36,9 +43,13 @@ class Configuration:
             logger.info(
                 f"Obtained emoji {block} with channel id -> {self.parser[block]['channel_id']}"
             )
+            message_format = str(self.parser[block]["message_format"])
+
             bookmarks[block] = {
                 "channel_id": int(self.parser[block]["channel_id"]),
-                "message_format": str(self.parser[block]["message_format"]),
+                "message_format": message_format
+                if message_format
+                else DEFAULT_MESSAGE_FORMAT,
             }
 
         return bookmarks
